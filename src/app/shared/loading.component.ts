1c31180d80fb39507b439c4cf67dda2ec9a46d0d
Meysam Sahragard <meysam.sahragard@gmail.com>
2021-04-12 03:14:46 +0430

    fix url

--- src/app/shared/loading.component.ts
+++ src/app/shared/loading.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-loading',
   template: `
-    <img alt="loading" src="https://webtml.ir/parspack/assets/loading.gif">`,
+    <img alt="loading" src="../../assets/loading.gif">`,
   styleUrls: ['loading.component.scss']
 })
 
